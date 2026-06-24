435fa1367f2f0c62269612272243a16f5ff7757b
okmic <okmic.g@gmail.com>
2026-06-24 16:11:17 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/MicoEngineer/',
+  base: '/PerfAuraCP/',
   build: {
     outDir: './build',
     emptyOutDir: true
